62237b3fc9d93d9761babc1c64778ec325ed75ce
slyk26 <102811034+slyk26@users.noreply.github.com>
2023-12-03 21:50:47 +0100

    day 1 bugfix

--- src/day1/part2.rs
+++ src/day1/part2.rs
@@ -1,34 +1,24 @@
-
-
 pub fn solve(input: &[String]) {
     let numbers_strings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     let mut sum = 0;
-    let mut a: Vec<(&str, i32)> = Vec::new();
+    let mut a: Vec<(usize, &str)> = Vec::new();
     
     for s in input {
         for number in numbers_strings {
-            s.match_indices(number).for_each(|(i, v)| a.push((v, i as i32)));
+            s.match_indices(number).for_each(|i| a.push(i));
         }
         
-        a.retain(|(_, v)| v.ne(&-1));
-        a.sort_by(|a, b| a.1.cmp(&b.1));
-        
-        let (fw, _) = a.first().unwrap();
-        let (lw, _) = a.last().unwrap();
-        
-        let add = format!("{}{}", pparse(fw), pparse(lw)
-        }).parse::<u32>().unwrap();
-        
-        sum += add;
+        a.sort_by(|a, b| a.0.cmp(&b.0));
+        sum += format!("{}{}", pparse(a.first().unwrap().1, &numbers_strings), pparse(a.last().unwrap().1, &numbers_strings)).parse::<usize>().unwrap();
         a = Vec::new();
     }
 
     println!("{sum}");
 }
 
-fn pparse(s: &&str) -> u32 {
-    match fw.parse::<u32>() {
+fn pparse(s: &str, numbers_strings: &[&str]) -> usize {
+    match s.parse::<usize>() {
         Ok(n) => n,
-        Err(_) => numbers_strings.iter().position(|n| n.eq(fw)).unwrap() as u32 +1
+        Err(_) => numbers_strings.iter().position(|n| n.eq(&s)).unwrap() +1
     }
 }
