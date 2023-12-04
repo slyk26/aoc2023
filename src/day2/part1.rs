af9149656ce24f1bcbd060cd11750b83e5a1a170
slyk26 <102811034+slyk26@users.noreply.github.com>
2023-12-04 12:55:43 +0100

    day 2 part 1 - nice

--- src/day2/part1.rs
+++ src/day2/part1.rs
@@ -1,34 +1,27 @@
+use std::collections::HashMap;
+
 pub fn solve(input: &[String]) {
-    let red_max = 12;
-    let green_max = 13;
-    let blue_max = 14;
     let mut sum = 0;
+    let mut max = HashMap::new();
+    max.insert("red", 12);
+    max.insert("green", 13);
+    max.insert("blue", 14);
     
     for (i, s) in input.iter().enumerate() {
-        let game = s.split(":").last().unwrap();
-        let cubes: Vec<_> = game.split(";").collect();
-        let mut red_possible = true;
-        let mut blue_possible = true;
-        let mut green_possible = true;
+        let game: Vec<_> = s.split(":").last().unwrap().split(";").collect();
+        let mut possible = true;
         
-        for cube in cubes {
-            let throw : Vec<_> = cube.trim().split(",").collect();
-            for c in throw {
-                let d: Vec<_> = c.trim().split(" ").collect();
-                let number = d.get(0).unwrap().parse::<i32>().unwrap();
-                let color = d.get(1).unwrap();
-                let _ = match color {
-                    &"red" => red_possible &= number <= red_max,
-                    &"green" => green_possible &= number <= green_max,
-                    &"blue" => blue_possible &= number <= blue_max,
-                    _ => ()
-                };
+        for turn in game {
+            let cubes : Vec<_> =  turn.trim().split(",").collect();
+            for cube in cubes {
+                let mut cube_info = cube.trim().split(" ").into_iter();
+                let grab = (cube_info.next().unwrap().parse::<i32>().unwrap(), cube_info.next().unwrap());
+                possible &=  grab.0 <= *max.get(grab.1).unwrap();
             }
         }
-        if red_possible && green_possible && blue_possible {
+        if possible {
             sum += i+1
         }
     }
-    
     println!("{sum}");
 }
